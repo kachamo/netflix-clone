d2f57b631a575616b29e7afeb601fa13e6c7dac8
KACHAMO LANKAMO <kachamotsegaye@gmail.com>
2023-04-22 09:34:43 -0400

    profile page.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
-import { Switch, Route, Router } from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() { 
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  useEffect(() =>{
-    const unsubscribe = auth.onAuthStateChanged(userAuth =>{
-      if (userAuth){
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
         // Logged in
         console.log(userAuth);
-        dispatch(login)
-        login({
+        dispatch(login({
           uid: userAuth.uid,
           email: userAuth.email,
-        })
-      }else{
-        // Logged Out
-        dispatch(logout);
+        }));
+      } else {
+        // Logged out
+        dispatch(logout());
       }
     });
+  
     return unsubscribe;
-  })
+  }, [dispatch]);
     return (
       <div className="App">
-        <Router>
-            {!user ? (
-              <LoginScreen />
-            ) : (
-              <Switch> 
-                <Route 
-                  path="/" element={<HomeScreen />}>
-                </Route>
-              </Switch>             
-            )}
-        </Router>
-      </div>
-    );
-  }
+        <Routes>
+          {!user ? (
+            <Route path="/" element={<LoginScreen />} />
+          ) : (
+            <Route path="/" element={<AuthenticatedRoutes />} />
+          )}
+        </Routes>
+    </div>
+  );
+}
+
+function AuthenticatedRoutes() {
+  return (    
+    <Routes>
+      <Route path='/' element={<HomeScreen />} />
+      <Route path='/profile' element={<ProfileScreen />} />
+    </Routes>
+  );
+}
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import "./Nav.css";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const navigate = useNavigate();
+  
   const transitionNavBar = ()=>{
     if (window.scrollY > 100){
       handleShow(true)
@@ -10,19 +13,25 @@ function Nav() {
       handleShow(false)
     }
   }
+  
   useEffect(() => {
       window.addEventListener('scroll', transitionNavBar);
-      return ()=> window.removeEventListener("scrool", transitionNavBar)
-    }
-  )
+      return ()=> window.removeEventListener("scroll", transitionNavBar)
+    }, []); 
+    
   return (
     <div className={`nav ${show && 'nav__black'}`}>
         <div className='nav__contents'>
-            <img className='nav__logo' src={ require('./Assets/Logo.png')} alt='Netflix Logo' />
-            <img className='nav__avatar' src={ require('./Assets/avatarOne.png')} alt='Netflix Logo' />
+    
+        <Link to='/'>
+          <img className='nav__logo' src={require('./Assets/Logo.png')} alt='Netflix Logo' />
+        </Link>
+        <Link to='/profile'>
+          <img onClick={()=> navigate('/profile')} className='nav__avatar' src={require('./Assets/avatarOne.png')} alt='Netflix Logo' />
+        </Link>  
         </div>
     </div>
-  )
+  );
 }
 
 export default Nav;
